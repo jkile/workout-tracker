df14c836a2d2ce05d6f6af35cd67d36cc53cb4f6
Jake Kile <jkile9@gmail.com>
2020-03-27 02:00:29 -0700

    setup route boilerplate

---
+++ routes/apiRoutes.js
@@ -0,0 +1,19 @@
+const db = require("../models");
+
+module.exports = function(app){
+    app.get("/api/workouts", function(req, res){
+
+    });
+
+    app.put("/api/workouts/:id", function(req, res){
+
+    });
+
+    app.post("/api/workouts/", function(req, res){
+
+    });
+
+    app.get("/api/workouts/range", function(req, res){
+
+    });
+}
